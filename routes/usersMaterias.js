3cbe2850f28ad4383836e31fbf32e20542d97bc3
Gustavo Arango <gustavo.arango@pacific-hs.com>
2020-04-07 14:34:12 -0500

    all

--- routes/usersMaterias.js
+++ routes/usersMaterias.js
@@ -11,16 +11,28 @@ function userMateriasApi(app) {
   const userMateriasService = new UserMateriasService()
 
   router.post('/all', async function (req, res, next){
-    
-    console.log(req.body);
 
+    
     
     const {data} = req.body
-
+   
     try {
-
       const usersMaterias = await userMateriasService.getUserMaterias( data )
+      res.status(200).json({
+        data: usersMaterias,
+        message: "materias listadas"
+      })
 
+    } catch (error) {
+      next(error)
+    }
+  })
+
+  router.post('/student', async function (req, res, next){
+    
+    const {data} = req.body
+    try {
+      const usersMaterias = await userMateriasService.getUserMateriasStudent( data )
       res.status(200).json({
         data: usersMaterias,
         message: "materias listadas"
--- services/usersMaterias.js
+++ services/usersMaterias.js
@@ -35,6 +35,49 @@ class UserMateriasService {
     return dataMaterias || []
   }
 
+  async getUserMateriasStudent(data) {
+
+
+   
+
+    const materias = await this.MongoDB.getAll('materias', {})
+    const userMaterias = await this.MongoDB.getAll(this.collection, data)
+
+    const dataMaterias = []
+    const dataMateriasEstudiante = []
+
+    for (let i = 0; i < materias.length; i++) {
+      for (let j = 0; j < userMaterias.length; j++) {
+     
+        if (materias[i]._id == userMaterias[j].id_subject)
+
+          dataMaterias.push({
+            id: userMaterias[j]._id,
+            name: materias[i].name,
+            code: materias[i].code,
+            credits: materias[i].credits,
+            group: userMaterias[j].group,
+            notes: userMaterias[j].notes,
+            notesnumber: userMaterias[j].notesnumber,
+            notesPheader: userMaterias[j].notesPheader,
+            notesPitem: userMaterias[j].notesPitem,
+       
+          })
+      }
+    }
+    for (let i = 0; i < dataMaterias.length; i++) {
+      for (let j = 0; j < dataMaterias[i].notesnumber.length; j++) {
+        if (dataMaterias[i].notesnumber[j].id== data) {
+          dataMateriasEstudiante.push(dataMaterias[i])
+        }else{
+          delete dataMaterias[i].notesnumber[j]
+        }
+      }
+    }
+    
+    return  dataMateriasEstudiante || [] 
+  }
+
   async createUserMaterias({ userMovie }) {
 
   }
